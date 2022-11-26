c890bdbe704658b72e14312900e4028c34019c65
RamiroHojman <ramirohojman@gmail.com>
2022-11-25 23:55:50 -0300

    A

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,4 @@
 ﻿document.getElementById('btn-imgusuario')?.click()
-function MostrarFotoUsuario(fotoUsuario){
-    $("#imgusuario").attr("src", "/img/"+fotoUsuario)
-
 function MostrarFotoUsuario(fotoUsuario, nombreUsuario){
     $("#imgusuario").attr("src", "/img/" + fotoUsuario) 
     $("#nombreusuario").html(nombreUsuario)
